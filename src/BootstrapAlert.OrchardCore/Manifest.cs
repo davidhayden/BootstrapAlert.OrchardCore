c21a31cef5876c77aeb52aebf81ba22aee5c243d
davidhayden <davidhayden@users.noreply.github.com>
2018-10-24 20:00:51 -0400

    Add initial files

---
+++ src/BootstrapAlert.OrchardCore/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "Bootstrap Alert",
+    Author = "David Hayden",
+    Website = "https://www.davidhayden.me",
+    Version = "2.0.0",
+    Description = "Adds a recipe that creates a Bootstrap Alert Widget.",
+    Category = "Content"
+)]
